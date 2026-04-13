af1a5c3b0cfffb83e3d434197a28ecdc7650ae6c
ST1026 <st10260942@vgconnect.edu.za>
2026-04-13 22:57:25 +0200

    Final Commit for now

--- DragonGame.GADE/Form1.Designer.cs
+++ DragonGame.GADE/Form1.Designer.cs
@@ -28,129 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            groupBox1 = new GroupBox();
-            groupBox2 = new GroupBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            dragonBox = new GroupBox();
-            iceDragon = new RadioButton();
-            fireDragon = new RadioButton();
-            windDragon = new RadioButton();
-            earthDragon = new RadioButton();
-            dragonBox2 = new GroupBox();
-            fireDragon2 = new RadioButton();
-            iceDragon2 = new RadioButton();
-            windDragon2 = new RadioButton();
-            earthDragon2 = new RadioButton();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            player1Info = new GroupBox();
             savePlayer1 = new Button();
+            dragonBox = new GroupBox();
+            earthDragon = new RadioButton();
+            windDragon = new RadioButton();
+            fireDragon = new RadioButton();
+            iceDragon = new RadioButton();
+            dragon1Name = new TextBox();
+            player1Name = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            player2Info = new GroupBox();
             savePlayer2 = new Button();
+            dragonBox2 = new GroupBox();
+            earthDragon2 = new RadioButton();
+            windDragon2 = new RadioButton();
+            iceDragon2 = new RadioButton();
+            fireDragon2 = new RadioButton();
+            dragon2Name = new TextBox();
+            player2Name = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
             startGame = new Button();
-            groupBox1.SuspendLayout();
-            groupBox2.SuspendLayout();
+            dragonStats = new RichTextBox();
+            player1Info.SuspendLayout();
             dragonBox.SuspendLayout();
+            player2Info.SuspendLayout();
             dragonBox2.SuspendLayout();
             SuspendLayout();
             // 
-            // groupBox1
+            // player1Info
             // 
-            groupBox1.Controls.Add(savePlayer1);
-            groupBox1.Controls.Add(dragonBox);
-            groupBox1.Controls.Add(textBox2);
-            groupBox1.Controls.Add(textBox1);
-            groupBox1.Controls.Add(label2);
-            groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(12, 45);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(330, 305);
-            groupBox1.TabIndex = 0;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "Player1";
+            player1Info.Controls.Add(savePlayer1);
+            player1Info.Controls.Add(dragonBox);
+            player1Info.Controls.Add(dragon1Name);
+            player1Info.Controls.Add(player1Name);
+            player1Info.Controls.Add(label2);
+            player1Info.Controls.Add(label1);
+            player1Info.Location = new Point(12, 45);
+            player1Info.Name = "player1Info";
+            player1Info.Size = new Size(302, 305);
+            player1Info.TabIndex = 0;
+            player1Info.TabStop = false;
+            player1Info.Text = "Player1";
             // 
-            // groupBox2
+            // savePlayer1
             // 
-            groupBox2.Controls.Add(savePlayer2);
-            groupBox2.Controls.Add(dragonBox2);
-            groupBox2.Controls.Add(textBox4);
-            groupBox2.Controls.Add(textBox3);
-            groupBox2.Controls.Add(label4);
-            groupBox2.Controls.Add(label3);
-            groupBox2.Location = new Point(348, 45);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(329, 305);
-            groupBox2.TabIndex = 1;
-            groupBox2.TabStop = false;
-            groupBox2.Text = "Player2";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(6, 27);
-            label1.Name = "label1";
-            label1.Size = new Size(77, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Player Name:";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(6, 71);
-            label2.Name = "label2";
-            label2.Size = new Size(87, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Dragon Name: ";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(6, 27);
-            label3.Name = "label3";
-            label3.Size = new Size(77, 15);
-            label3.TabIndex = 0;
-            label3.Text = "Player Name:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(6, 71);
-            label4.Name = "label4";
-            label4.Size = new Size(84, 15);
-            label4.TabIndex = 1;
-            label4.Text = "Dragon Name:";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(99, 24);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(222, 23);
-            textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(99, 68);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(222, 23);
-            textBox2.TabIndex = 3;
-            textBox2.TextChanged += textBox2_TextChanged;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(101, 24);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(222, 23);
-            textBox3.TabIndex = 2;
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(101, 68);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(222, 23);
-            textBox4.TabIndex = 3;
+            savePlayer1.Location = new Point(6, 269);
+            savePlayer1.Name = "savePlayer1";
+            savePlayer1.Size = new Size(290, 27);
+            savePlayer1.TabIndex = 5;
+            savePlayer1.Text = "Save";
+            savePlayer1.UseVisualStyleBackColor = true;
             // 
             // dragonBox
             // 
@@ -160,43 +91,10 @@
             dragonBox.Controls.Add(iceDragon);
             dragonBox.Location = new Point(6, 113);
             dragonBox.Name = "dragonBox";
-            dragonBox.Size = new Size(315, 150);
+            dragonBox.Size = new Size(290, 150);
             dragonBox.TabIndex = 4;
             dragonBox.TabStop = false;
             dragonBox.Text = "Dragon Type";
-            // 
-            // iceDragon
-            // 
-            iceDragon.AutoSize = true;
-            iceDragon.Location = new Point(13, 68);
-            iceDragon.Name = "iceDragon";
-            iceDragon.Size = new Size(82, 19);
-            iceDragon.TabIndex = 0;
-            iceDragon.TabStop = true;
-            iceDragon.Text = "Ice Dragon";
-            iceDragon.UseVisualStyleBackColor = true;
-            // 
-            // fireDragon
-            // 
-            fireDragon.AutoSize = true;
-            fireDragon.Location = new Point(13, 43);
-            fireDragon.Name = "fireDragon";
-            fireDragon.Size = new Size(86, 19);
-            fireDragon.TabIndex = 1;
-            fireDragon.TabStop = true;
-            fireDragon.Text = "Fire Dragon";
-            fireDragon.UseVisualStyleBackColor = true;
-            // 
-            // windDragon
-            // 
-            windDragon.AutoSize = true;
-            windDragon.Location = new Point(13, 93);
-            windDragon.Name = "windDragon";
-            windDragon.Size = new Size(95, 19);
-            windDragon.TabIndex = 2;
-            windDragon.TabStop = true;
-            windDragon.Text = "Wind Dragon";
-            windDragon.UseVisualStyleBackColor = true;
             // 
             // earthDragon
             // 
@@ -209,6 +107,96 @@
             earthDragon.Text = "Earth Dragon";
             earthDragon.UseVisualStyleBackColor = true;
             // 
+            // windDragon
+            // 
+            windDragon.AutoSize = true;
+            windDragon.Location = new Point(13, 93);
+            windDragon.Name = "windDragon";
+            windDragon.Size = new Size(95, 19);
+            windDragon.TabIndex = 2;
+            windDragon.TabStop = true;
+            windDragon.Text = "Wind Dragon";
+            windDragon.UseVisualStyleBackColor = true;
+            // 
+            // fireDragon
+            // 
+            fireDragon.AutoSize = true;
+            fireDragon.Location = new Point(13, 43);
+            fireDragon.Name = "fireDragon";
+            fireDragon.Size = new Size(86, 19);
+            fireDragon.TabIndex = 1;
+            fireDragon.TabStop = true;
+            fireDragon.Text = "Fire Dragon";
+            fireDragon.UseVisualStyleBackColor = true;
+            // 
+            // iceDragon
+            // 
+            iceDragon.AutoSize = true;
+            iceDragon.Location = new Point(13, 68);
+            iceDragon.Name = "iceDragon";
+            iceDragon.Size = new Size(82, 19);
+            iceDragon.TabIndex = 0;
+            iceDragon.TabStop = true;
+            iceDragon.Text = "Ice Dragon";
+            iceDragon.UseVisualStyleBackColor = true;
+            // 
+            // dragon1Name
+            // 
+            dragon1Name.Location = new Point(99, 68);
+            dragon1Name.Name = "dragon1Name";
+            dragon1Name.Size = new Size(197, 23);
+            dragon1Name.TabIndex = 3;
+            dragon1Name.TextChanged += textBox2_TextChanged;
+            // 
+            // player1Name
+            // 
+            player1Name.Location = new Point(99, 24);
+            player1Name.Name = "player1Name";
+            player1Name.Size = new Size(197, 23);
+            player1Name.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Dragon Name: ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Player Name:";
+            // 
+            // player2Info
+            // 
+            player2Info.Controls.Add(savePlayer2);
+            player2Info.Controls.Add(dragonBox2);
+            player2Info.Controls.Add(dragon2Name);
+            player2Info.Controls.Add(player2Name);
+            player2Info.Controls.Add(label4);
+            player2Info.Controls.Add(label3);
+            player2Info.Location = new Point(337, 45);
+            player2Info.Name = "player2Info";
+            player2Info.Size = new Size(302, 305);
+            player2Info.TabIndex = 1;
+            player2Info.TabStop = false;
+            player2Info.Text = "Player2";
+            // 
+            // savePlayer2
+            // 
+            savePlayer2.Location = new Point(6, 269);
+            savePlayer2.Name = "savePlayer2";
+            savePlayer2.Size = new Size(290, 27);
+            savePlayer2.TabIndex = 5;
+            savePlayer2.Text = "Save";
+            savePlayer2.UseVisualStyleBackColor = true;
+            // 
             // dragonBox2
             // 
             dragonBox2.Controls.Add(earthDragon2);
@@ -217,43 +205,10 @@
             dragonBox2.Controls.Add(fireDragon2);
             dragonBox2.Location = new Point(6, 113);
             dragonBox2.Name = "dragonBox2";
-            dragonBox2.Size = new Size(317, 150);
+            dragonBox2.Size = new Size(290, 150);
             dragonBox2.TabIndex = 4;
             dragonBox2.TabStop = false;
             dragonBox2.Text = "Dragon Type";
-            // 
-            // fireDragon2
-            // 
-            fireDragon2.AutoSize = true;
-            fireDragon2.Location = new Point(16, 43);
-            fireDragon2.Name = "fireDragon2";
-            fireDragon2.Size = new Size(86, 19);
-            fireDragon2.TabIndex = 0;
-            fireDragon2.TabStop = true;
-            fireDragon2.Text = "Fire Dragon";
-            fireDragon2.UseVisualStyleBackColor = true;
-            // 
-            // iceDragon2
-            // 
-            iceDragon2.AutoSize = true;
-            iceDragon2.Location = new Point(16, 68);
-            iceDragon2.Name = "iceDragon2";
-            iceDragon2.Size = new Size(82, 19);
-            iceDragon2.TabIndex = 1;
-            iceDragon2.TabStop = true;
-            iceDragon2.Text = "Ice Dragon";
-            iceDragon2.UseVisualStyleBackColor = true;
-            // 
-            // windDragon2
-            // 
-            windDragon2.AutoSize = true;
-            windDragon2.Location = new Point(16, 93);
-            windDragon2.Name = "windDragon2";
-            windDragon2.Size = new Size(95, 19);
-            windDragon2.TabIndex = 2;
-            windDragon2.TabStop = true;
-            windDragon2.Text = "Wind Dragon";
-            windDragon2.UseVisualStyleBackColor = true;
             // 
             // earthDragon2
             // 
@@ -266,49 +221,106 @@
             earthDragon2.Text = "Earth Dragon";
             earthDragon2.UseVisualStyleBackColor = true;
             // 
-            // savePlayer1
+            // windDragon2
             // 
-            savePlayer1.Location = new Point(6, 269);
-            savePlayer1.Name = "savePlayer1";
-            savePlayer1.Size = new Size(315, 27);
-            savePlayer1.TabIndex = 5;
-            savePlayer1.Text = "Save";
-            savePlayer1.UseVisualStyleBackColor = true;
+            windDragon2.AutoSize = true;
+            windDragon2.Location = new Point(16, 93);
+            windDragon2.Name = "windDragon2";
+            windDragon2.Size = new Size(95, 19);
+            windDragon2.TabIndex = 2;
+            windDragon2.TabStop = true;
+            windDragon2.Text = "Wind Dragon";
+            windDragon2.UseVisualStyleBackColor = true;
             // 
-            // savePlayer2
+            // iceDragon2
             // 
-            savePlayer2.Location = new Point(6, 269);
-            savePlayer2.Name = "savePlayer2";
-            savePlayer2.Size = new Size(317, 27);
-            savePlayer2.TabIndex = 5;
-            savePlayer2.Text = "Save";
-            savePlayer2.UseVisualStyleBackColor = true;
+            iceDragon2.AutoSize = true;
+            iceDragon2.Location = new Point(16, 68);
+            iceDragon2.Name = "iceDragon2";
+            iceDragon2.Size = new Size(82, 19);
+            iceDragon2.TabIndex = 1;
+            iceDragon2.TabStop = true;
+            iceDragon2.Text = "Ice Dragon";
+            iceDragon2.UseVisualStyleBackColor = true;
+            // 
+            // fireDragon2
+            // 
+            fireDragon2.AutoSize = true;
+            fireDragon2.Location = new Point(16, 43);
+            fireDragon2.Name = "fireDragon2";
+            fireDragon2.Size = new Size(86, 19);
+            fireDragon2.TabIndex = 0;
+            fireDragon2.TabStop = true;
+            fireDragon2.Text = "Fire Dragon";
+            fireDragon2.UseVisualStyleBackColor = true;
+            // 
+            // dragon2Name
+            // 
+            dragon2Name.Location = new Point(101, 68);
+            dragon2Name.Name = "dragon2Name";
+            dragon2Name.Size = new Size(195, 23);
+            dragon2Name.TabIndex = 3;
+            // 
+            // player2Name
+            // 
+            player2Name.Location = new Point(101, 24);
+            player2Name.Name = "player2Name";
+            player2Name.Size = new Size(195, 23);
+            player2Name.TabIndex = 2;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 71);
+            label4.Name = "label4";
+            label4.Size = new Size(84, 15);
+            label4.TabIndex = 1;
+            label4.Text = "Dragon Name:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 27);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Player Name:";
             // 
             // startGame
             // 
-            startGame.Location = new Point(12, 372);
+            startGame.Location = new Point(12, 384);
             startGame.Name = "startGame";
-            startGame.Size = new Size(665, 40);
+            startGame.Size = new Size(627, 40);
             startGame.TabIndex = 2;
             startGame.Text = "Start Game";
             startGame.UseVisualStyleBackColor = true;
+            // 
+            // dragonStats
+            // 
+            dragonStats.BackColor = SystemColors.ButtonFace;
+            dragonStats.Location = new Point(674, 12);
+            dragonStats.Name = "dragonStats";
+            dragonStats.Size = new Size(127, 374);
+            dragonStats.TabIndex = 4;
+            dragonStats.Text = resources.GetString("dragonStats.Text");
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 424);
+            ClientSize = new Size(813, 436);
+            Controls.Add(dragonStats);
             Controls.Add(startGame);
-            Controls.Add(groupBox2);
-            Controls.Add(groupBox1);
+            Controls.Add(player2Info);
+            Controls.Add(player1Info);
             Name = "Form1";
             Text = "Form1";
-            groupBox1.ResumeLayout(false);
-            groupBox1.PerformLayout();
-            groupBox2.ResumeLayout(false);
-            groupBox2.PerformLayout();
+            player1Info.ResumeLayout(false);
+            player1Info.PerformLayout();
             dragonBox.ResumeLayout(false);
             dragonBox.PerformLayout();
+            player2Info.ResumeLayout(false);
+            player2Info.PerformLayout();
             dragonBox2.ResumeLayout(false);
             dragonBox2.PerformLayout();
             ResumeLayout(false);
@@ -316,16 +328,16 @@
 
         #endregion
 
-        private GroupBox groupBox1;
+        private GroupBox player1Info;
         private Label label2;
         private Label label1;
-        private GroupBox groupBox2;
+        private GroupBox player2Info;
         private Label label4;
         private Label label3;
-        private TextBox textBox2;
-        private TextBox textBox1;
-        private TextBox textBox4;
-        private TextBox textBox3;
+        private TextBox dragon1Name;
+        private TextBox player1Name;
+        private TextBox dragon2Name;
+        private TextBox player2Name;
         private GroupBox dragonBox;
         private RadioButton earthDragon;
         private RadioButton windDragon;
@@ -339,5 +351,6 @@
         private Button savePlayer1;
         private Button savePlayer2;
         private Button startGame;
+        private RichTextBox dragonStats;
     }
 }
