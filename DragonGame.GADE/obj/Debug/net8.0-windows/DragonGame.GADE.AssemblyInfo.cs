858dafb606bd927d39391d3c84c85e3acd8a9750
ST1026 <st10260942@vgconnect.edu.za>
2026-06-17 13:29:23 +0200

    Extras

--- DragonGame.GADE/obj/Debug/net8.0-windows/DragonGame.GADE.AssemblyInfo.cs
+++ DragonGame.GADE/obj/Debug/net8.0-windows/DragonGame.GADE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DragonGame.GADE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fe1484189463b3c262bbc87513dcf5c4dc72a07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349e9ed6deee88d2bd1a650ed95b434fc9c29314")]
 [assembly: System.Reflection.AssemblyProductAttribute("DragonGame.GADE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DragonGame.GADE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
